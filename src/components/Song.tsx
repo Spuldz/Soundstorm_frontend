d24f3367bf4e91b220bafd9c51d2f74fb9d5987d
Spuldz <mozgaedvards0@gmail.com>
2023-08-07 21:04:06 +0300

    audio sending optimization

--- src/components/Song.tsx
+++ src/components/Song.tsx
@@ -11,25 +11,39 @@ export const Song = (props: {data?:ISong, handleInteraction:Function}) => {
     const [audio, setAudio] = useState<any>()
     const [playing, setPlaying] = useState<boolean>()
 
-    useEffect(() => {
-        let audioURL:any;
-        axios.get(`${appData.apiUrl}/song/getAudio/${props.data?.fileName}`, {
-            responseType: 'blob',
-            headers:{
-                "Authorization": `Bearer ${cookies.accessToken}`
-            }
-        })
-        .then(res => {
-            audioURL = URL.createObjectURL(res.data)
-            setAudio(new Audio(audioURL))
-        })
+  
 
-    }, [])
-
+    function getSong(){
+        
+        return new Promise((resolve, reject) => {
+            axios.get(`${appData.apiUrl}/song/getAudio/${props.data?.fileName}`, {
+                responseType: 'blob',
+                headers:{
+                    "Authorization": `Bearer ${cookies.accessToken}`
+                }
+            })
+            .then(res => {
+                const audioURL = URL.createObjectURL(res.data)
+                resolve(new Audio(audioURL))
+            }).catch(
+                err => {
+                    reject(err)
+                }
+            )
+        })
+    }
 
 
     return(
-        <div className={styles.main} onClick={() => props.handleInteraction(audio)}>
+        <div className={styles.main} onClick={async () => {
+            if(typeof audio === "undefined"){
+                const song = await getSong()
+                setAudio(song)
+                props.handleInteraction(song)
+            }else{
+                props.handleInteraction(audio)
+            }
+        }}>
             <div className={styles.img} style={{backgroundImage: `URL(${props.data?.thumbnail})`}}></div>
             <div className={styles.data}>
                 <span className={styles.artist}>{props.data?.ownerName}</span>
